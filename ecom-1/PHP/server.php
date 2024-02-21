59ba1b8a1522421d12c44bbcd5e0adbc04400b20
santiagu-gap <santiagu.gap@gmail.com>
2024-02-21 08:05:02 -0500

    Managed to integrate PHP with React Node
    We won't use this but it's good to have it just in case

--- ecom-1/PHP/server.php
+++ ecom-1/PHP/server.php
@@ -1,7 +1,7 @@
 // FileName - server.php
 
 <?php
-	header('Access-Control-Allow-Origin: http://localhost:3000');
+	header('Access-Control-Allow-Origin: http://localhost:3001');
 	$user = $_POST['name'];
 	echo ("Hello from server: $user");
 ?>
--- ecom-1/src/App.js
+++ ecom-1/src/App.js
@@ -1,23 +1,38 @@
 import logo from './logo.svg';
+import React, { useState } from 'react';
+import $ from "jquery";
 import './App.css';
 
 function App() {
+  const [name, setName] = useState("");
+  const [result, setResult] = useState("");
+
+  const handleChange = (e) => {
+    setName(e.target.value);
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = $(e.target);
+    $.ajax({
+      type: "POST",
+      url: form.attr("action"),
+      data: form.serialize(),
+      success(data) {
+        setResult(data);
+      }
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form action="http://localhost:80" method="POST" onSubmit={(event) => handleSubmit(event)}>
+        <input type="text" id="name" name="name" value={name} onChange={(event) => { handleChange(event) }} />
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+
+      <h1>{result}</h1>
     </div>
   );
 }

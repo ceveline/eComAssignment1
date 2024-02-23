4632d72f88b60ff3d86cbb326c6ced40f30a5a9a
santiagu-gap <santiagu.gap@gmail.com>
2024-02-23 08:45:22 -0500

    Reformated the whole views and App
    Also made it such that routes can match without worrying about capitals

--- webapp/app/core/App.php
+++ webapp/app/core/App.php
@@ -1,27 +1,37 @@
 <?php
+
 namespace app\core;
 
-class App{
-    
-    function __construct(){
+class App
+{
 
-    $url = $_GET['url'];
+    function __construct()
+    {
+
+        $url = $_GET['url'];
 
         //defined a few routes "url"=>"controller,method"
-        $routes = ['Contact/' => 'Contact,landing',
-                    'Contact/about' => 'Contact,about',
-                    'Contact/read' => 'Contact,loadMessagePage', //load the view
-                    'Contact/confirm_read'=> 'Contact,writeMessage', //re confirm the view page
-                    'Contact/contact_us'=> 'Contact,loadContactPage',
-                    
-    ];
+        $routes = [
+            //School instructions
+            'Main/index' => 'Contact,index',
+            'Main/about_us' => 'Contact,about_us',
+            'Contact/read' => 'Contact,loadMessagePage',
+            'Contact/index' => 'Contact,loadContactPage',
+            //Missing Count/index
+
+            //Extra routes for simplicity
+            '' => 'Contact,index',
+            'Contact/' => 'Contact,loadContactPage',
+            'Contact/confirm_read' => 'Contact,writeMessage', //re confirm the view page
+            'Contact/contact_us' => 'Contact,loadContactPage',
+        ];
 
         //one by one compare the url to resolve the route
         foreach ($routes as $routeUrl => $controllerMethod) {
-            if($url == $routeUrl){//match the route
+            if (strtolower($url) == strtolower($routeUrl)) { //match the route
                 //run the route
-                [$controller,$method]=explode(',', $controllerMethod);
-                $controller = '\\app\\controllers\\'.$controller;
+                [$controller, $method] = explode(',', $controllerMethod);
+                $controller = '\\app\\controllers\\' . $controller;
                 $controller = new $controller();
                 $controller->$method();
                 //make sure that we don't run a second route
